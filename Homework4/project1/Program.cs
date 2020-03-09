7a0c59ee4ff55017dd2c0261d0c54993ff388b94
JoeJRW <1229858072@qq.com>
2020-03-09 20:07:33 +0800

    submit Homework4

---
+++ Homework4/project1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+/*
+*1、为示例中的泛型链表类添加
+*类似于List<T>类的ForEach(Action<T> action)方法。
+*通过调用这个方法打印链表元素，
+*求最大值、最小值和求和
+*（使用lambda表达式实现）。*/
+
+namespace project1
+{
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+
+        public void Foreach(Action<T> action)//新增
+        {
+            for (Node<T> node = Head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, max = int.MinValue, min = int.MaxValue;
+            GenericList<int> intlist = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                intlist.Add(i);
+            }
+
+            intlist.Foreach(x => Console.WriteLine(x.ToString()));
+            intlist.Foreach(x => sum += x);
+            Console.WriteLine($"Sum={sum}");
+            intlist.Foreach(delegate (int x) { max = (max > x) ? max : x; });
+            Console.WriteLine($"Max={max}");
+            intlist.Foreach(x => min = (min < x) ? min : x);
+            Console.WriteLine($"Min={min}");
+        }
+    }
+}
---
+++ Homework4/project2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Threading;
+/*2、使用事件机制，
+* 模拟实现一个闹钟功能。
+* 闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+* 在闹钟走时时或者响铃时，在控制台显示提示信息。*/
+
+namespace project2
+{
+    public delegate void TickHandler(object sender, ClockArgs e);
+    public delegate void AlarmHandler(object sender, ClockArgs e);
+
+    public class ClockArgs
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+    }
+
+    class MyClock
+    {
+        public event TickHandler TickEvent;
+        public event AlarmHandler AlarmEvent;
+        public int AlarmHour { get; set; }
+        public int AlarmMinute { get; set; }
+        public int AlarmSecond { get; set; }
+
+        public void TimeWentBy()
+        {
+            while (true)
+            {
+                int hour = DateTime.Now.Hour;
+                int minute = DateTime.Now.Minute;
+                int second = DateTime.Now.Second;
+                ClockArgs args = new ClockArgs()
+                {
+                    Hour = hour,
+                    Minute = minute,
+                    Second = second
+                };
+                TickEvent(this, args);//触发tick事件
+                if (AlarmHour == args.Hour && AlarmMinute == args.Minute && AlarmSecond == args.Second)
+                    AlarmEvent(this, args);//触发alarm事件
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void SetAlarm(int hour, int minute, int second)
+        {
+            AlarmHour = hour;
+            AlarmMinute = minute;
+            AlarmSecond = second;
+        }
+    }
+
+    class ClockListener
+    {
+        public MyClock myClock = new MyClock();
+        public ClockListener()
+        {
+            myClock.TickEvent += Tick;
+            myClock.AlarmEvent += Alarm;
+        }
+
+        void Tick(object sender, ClockArgs args)
+        {
+            Console.WriteLine("DIDA~");
+        }
+
+        void Alarm(object sender, ClockArgs args)
+        {
+            Console.WriteLine("dilinglingling~~~" +
+                $"It's {args.Hour}:{args.Minute}:{args.Second} right now");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockListener clockListener = new ClockListener();
+            clockListener.myClock.SetAlarm(11, 52, 50);//自己修改
+            clockListener.myClock.TimeWentBy();
+        }
+    }
+}
